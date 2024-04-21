83c7983d3a602d9746df35b2eae506b13652a0b2
faissal20 <fayssalz15z@gmail.com>
2024-04-21 14:49:27 +0100

    first commit

---
+++ app/Console/Commands/CheckSalatTiming.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Events\SalatTime;
+use Illuminate\Console\Command;
+
+class CheckSalatTiming extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:check-salat-timing';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+
+        
+        $salatTimes = config('static-salat-timing');
+
+        foreach ($salatTimes as $location => $time) {
+            foreach ($time as $salat => $timing) {
+                $now = now()->format('H:i');
+                if ($now === $timing) {
+                    event(new SalatTime($location, [
+                        'salat' => $salat,
+                        'timing' => $timing,
+                    ]));
+                }
+            }
+        }
+    }
+}
---
+++ config/static-salat-timing.php
@@ -0,0 +1,41 @@
+<?php
+
+// static time for salat on each city in Morocco
+
+return [
+    'Rabat' => [
+        'Fajr' => '05:00',
+        'Dhuhr' => '13:00',
+        'Asr' => '16:00',
+        'Maghrib' => '19:00',
+        'Isha' => '21:00',
+    ],
+    'Casablanca' => [
+        'Fajr' => '05:00',
+        'Dhuhr' => '13:00',
+        'Asr' => '16:00',
+        'Maghrib' => '19:00',
+        'Isha' => '21:00',
+    ],
+    'Tanger' => [
+        'Fajr' => '05:00',
+        'Dhuhr' => '13:00',
+        'Asr' => '16:00',
+        'Maghrib' => '19:00',
+        'Isha' => '21:00',
+    ],
+    'Tetouan' => [
+        'Fajr' => '05:00',
+        'Dhuhr' => '13:00',
+        'Asr' => '16:00',
+        'Maghrib' => '19:00',
+        'Isha' => '21:00',
+    ],
+    'Jebha' => [
+        'Fajr' => '05:00',
+        'Dhuhr' => '13:00',
+        'Asr' => '16:00',
+        'Maghrib' => '19:00',
+        'Isha' => '21:00',
+    ],
+];
